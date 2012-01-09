e5db3db2dcf1308817e5edea2f446f8fe9825891
Brad Proctor <brad@bradleyproctor.com>
2012-01-09 11:06:13 -0500

    Bump to version 1.4

--- password.php
+++ password.php
@@ -13,7 +13,7 @@
  * @license     MIT License (http://www.opensource.org/licenses/mit-license.php)
  * @link        http://bradleyproctor.com/
  * @since       1.0
- * @version     1.3
+ * @version     1.4
  */
 /*
  * Required values:
