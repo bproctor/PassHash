3985067485aba35804503b4178aae75dbc8cabd4
Brad Proctor <brad@bradleyproctor.com>
2012-08-18 22:57:27 -0700

    PSR-2 requires spaces instead of tabs :(

--- passhash.php
+++ passhash.php
@@ -37,105 +37,105 @@
 abstract class Passhash
 {
 
-	/**
-	 * Number of characters in the salt value
-	 */
-	const saltLength = 16;
+    /**
+     * Number of characters in the salt value
+     */
+    const saltLength = 16;
 
-	/**
-	 * a unique site-wide value to compliment the unique salts
-	 */
-	const authSalt = 'jS#W_;[;sjiNOUc9NG,S3T76NOTmK~%mu|#WI9-v.l^Bt]6H)1wz:kc=hPtS+JZv)haB!0dTo}klfWrr';
+    /**
+     * a unique site-wide value to compliment the unique salts
+     */
+    const authSalt = 'jS#W_;[;sjiNOUc9NG,S3T76NOTmK~%mu|#WI9-v.l^Bt]6H)1wz:kc=hPtS+JZv)haB!0dTo}klfWrr';
 
-	/**
-	 * Used for key stretching.  It is used to calculate the number of iterations to run the
- 	 * hashing algorithm. Raise this to increase security, lower this to make it run faster.  Default value
+    /**
+     * Used for key stretching.  It is used to calculate the number of iterations to run the
+     * hashing algorithm. Raise this to increase security, lower this to make it run faster.  Default value
      * is 5.
      */
-	const authLevel = 5;
+    const authLevel = 5;
 
-	/**
-	 * Generate a password salt
-	 *
-	 * @param int $length
-	 *    The number of characters that the salt should be
-	 *
-	 * @return string
-	 *    Returns a salt that can be used to salt a password hash
-	 *
-	 * @access private
-	 */
-	final private static function salt()
-	{
-		$salt = '';
-		while (strlen($salt) < static::saltLength) {
-			$salt .= pack('C', dechex(mt_rand()));
-		}
-		return substr(base64_encode($salt), 0, static::saltLength);
-	}
+    /**
+     * Generate a password salt
+     *
+     * @param int $length
+     *    The number of characters that the salt should be
+     *
+     * @return string
+     *    Returns a salt that can be used to salt a password hash
+     *
+     * @access private
+     */
+    final private static function salt()
+    {
+        $salt = '';
+        while (strlen($salt) < static::saltLength) {
+            $salt .= pack('C', dechex(mt_rand()));
+        }
+        return substr(base64_encode($salt), 0, static::saltLength);
+    }
 
-	/**
-	 * PBKDF2 Implementation (described in RFC 2898)
-	 * Password-Based Key Derivation Function
-	 * (Simplified, since some variables are known)
-	 *
-	 * @param string $password
-	 *      The plain text password
-	 *
-	 * @param string $salt
-	 *      The salt used to generate the hash
-	 *
-	 * @return string
-	 *		Derived key
-	 *
-	 * @access private
-	 */
-	final private static function pbkdf2($password, $salt)
-	{
-		$ib = $b = hash_hmac('whirlpool', $salt . static::authSalt, $password, true);
-		for ($i = 1; $i < static::authLevel * 1000; $i++) {
-			$ib ^= ($b = hash_hmac('whirlpool', $b . static::authSalt, $password, true));
-		}
-		return base64_encode($ib);
-	}
+    /**
+     * PBKDF2 Implementation (described in RFC 2898)
+     * Password-Based Key Derivation Function
+     * (Simplified, since some variables are known)
+     *
+     * @param string $password
+     *      The plain text password
+     *
+     * @param string $salt
+     *      The salt used to generate the hash
+     *
+     * @return string
+     *      Derived key
+     *
+     * @access private
+     */
+    final private static function pbkdf2($password, $salt)
+    {
+        $ib = $b = hash_hmac('whirlpool', $salt . static::authSalt, $password, true);
+        for ($i = 1; $i < static::authLevel * 1000; $i++) {
+            $ib ^= ($b = hash_hmac('whirlpool', $b . static::authSalt, $password, true));
+        }
+        return base64_encode($ib);
+    }
 
-	/**
-	 * Generate a 104 character password hash
-	 *
-	 * @param string $password
-	 *    The plain text password
-	 *
-	 * @param string $salt
-	 *    The salt to use to generate the password
-	 *
-	 * @return string
-	 *    Returns the 104-character hashed and salted password
-	 *
-	 * @access public
-	 */
-	final public static function hash($password, $salt = null)
-	{
-		$salt or $salt = static::salt();
-		return $salt . static::pbkdf2($password, $salt);
-	}
+    /**
+     * Generate a 104 character password hash
+     *
+     * @param string $password
+     *    The plain text password
+     *
+     * @param string $salt
+     *    The salt to use to generate the password
+     *
+     * @return string
+     *    Returns the 104-character hashed and salted password
+     *
+     * @access public
+     */
+    final public static function hash($password, $salt = null)
+    {
+        $salt or $salt = static::salt();
+        return $salt . static::pbkdf2($password, $salt);
+    }
 
-	/**
-	 * Compare a password with a hash
-	 *
-	 * @param string $password
-	 *    The plain text password to compare
-	 *
-	 * @param string $hash
-	 *    The 104 character password hash
-	 *
-	 * @return bool
-	 *    Returns TRUE if the password matches, FALSE if not
-	 *
-	 * @access public
-	 */
-	final public static function compare($password, $hash)
-	{
-		return 0 === strcmp($hash, static::hash($password, substr($hash, 0, static::saltLength), static::authLevel));
-	}
+    /**
+     * Compare a password with a hash
+     *
+     * @param string $password
+     *    The plain text password to compare
+     *
+     * @param string $hash
+     *    The 104 character password hash
+     *
+     * @return bool
+     *    Returns TRUE if the password matches, FALSE if not
+     *
+     * @access public
+     */
+    final public static function compare($password, $hash)
+    {
+        return 0 === strcmp($hash, static::hash($password, substr($hash, 0, static::saltLength), static::authLevel));
+    }
 
 }
